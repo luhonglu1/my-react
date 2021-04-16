bab6ae54c61bfbd13994528b3e5c3d4bf26c861f
好想告诉你loe <839859293@qq.com>
2021-04-16 18:33:15 +0800

    fix:查询字符串

--- src/components/admin/dashboard.jsx
+++ src/components/admin/dashboard.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React from 'react'
+import SideBar from './sidebar.jsx'
+import Users from './users'
+import Posts from './posts'
+import { Route } from 'react-router-dom'
 
 const Dashboard = ({ match }) => {
   return (
     <div>
       <h1>Admin Dashboard</h1>
+      <SideBar></SideBar>
+      <Route path="/admin/users" component={Users}></Route>
+      <Route path="/admin/Posts" component={Posts}></Route>
     </div>
-  );
-};
+  )
+}
 
-export default Dashboard;
+export default Dashboard
